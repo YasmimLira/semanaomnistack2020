f1f84ab8c91ed10dc814ddbdfd6d736378c787db
yasmim <micayaya70@gmail.com>
2020-03-26 21:16:38 -0300

    Front-end da aplicaçao

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -19,7 +19,8 @@ export default function Logon() {
         try {
             const response = await api.post('sessions', { id });
 
-            
+            localStorage.setItem('ongId', id);
+            localStorage.setItem('ongName', response.data.name)
 
             history.push('/profile')
         }catch (err) {
